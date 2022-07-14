fee4198de52c51844272011f09b29890b83105ca
Kauane Cordeiro <kauane@pgmais.com.br>
2022-07-14 19:48:00 -0300

    openapi

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi_utils.timing import add_timing_middleware
-
+from fastapi.openapi.utils import get_openapi
 from app.routes.fastapi_v1 import api_router_v1
 
 
@@ -22,6 +22,17 @@ def create_app():
     def custom_openapi():
         if app.openapi_schema:
             return app.openapi_schema
+        openapi_schema = get_openapi(
+            title="Custom title",
+            version="2.5.0",
+            description="This is a very custom OpenAPI schema",
+            routes=app.routes,
+        )
+        openapi_schema["info"]["x-logo"] = {
+            "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+        }
+        app.openapi_schema = openapi_schema
+        return app.openapi_schema
 
     app.include_router(api_router_v1, prefix="/v1")
 
